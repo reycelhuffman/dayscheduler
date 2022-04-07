6972f6abef5438d99dcdcb1559619312ebe5e968
Reycel Huffman <reycel.huffman@outlook.com>
2022-04-07 00:36:50 -0500

    working process

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-var timeDisplayEl = $('#time-display');
+let store = $("localStorage");
+let jumbotron = $("jumbotron");
+let now = ("moment");
+let timeDisplayEl = $('#time-display');
+let button = $("saveButton");
+let textEl = $("formControl");
+let rightNow = $("ShowDate");
+
 
 function displayTime() {
-    var rightNow =moment().format('LLLL');
+    let rightNow =moment().format('LLLL');
     timeDisplayEl.text(rightNow);
     console.log(timeDisplayEl);
 }
 setInterval(displayTime, 1000)
 
+function doSomethingn(){
+    let button = 
+    console.log(button);
+
+}
