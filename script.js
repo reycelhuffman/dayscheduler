293d4f03eff64d83af0a527aac0a04fc3b88e581
Reycel Huffman <reycel.huffman@outlook.com>
2022-04-07 19:43:51 -0500

    worked on css

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
-let store = $("localStorage");
 let jumbotron = $("jumbotron");
-let now = ("moment");
+//let now = $("moment");
+let currentDay = $("")
 let timeDisplayEl = $('#time-display');
-let button = $("saveButton");
-let textEl = $("formControl");
+//let button = $(".saveBtn");
 let rightNow = $("ShowDate");
+let containerEl = document.querySelector(".container");
+console.log(containerEl);
 
 
 function displayTime() {
-    let rightNow =moment().format('LLLL');
+    let rightNow = moment().format('LLLL');
     timeDisplayEl.text(rightNow);
-    console.log(timeDisplayEl);
+
 }
 setInterval(displayTime, 1000)
 
-function doSomethingn(){
-    let button = 
-    console.log(button);
-
+containerEl.addEventListener("click", function(event){
+    if (event.target.matches("button")) {
+        console.log(event.target.id);
+        console.log(event.target.parentElement.previousElements.value);
+        localStorage.setItem(event.target.id, event.parentElement.previousElementSibling.value);
+    }
+})
+function renderLastRegistered(){
+    let userSubmit = localStorage.getItem("9");
+    let nine = document.getElementById("nine");
+    nine.innerHTML = userSubmit;
+}
+window.onload-function() {
+    renderLastRegistered();
 }
